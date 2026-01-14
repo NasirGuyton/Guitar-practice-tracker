2c94474717c5d312b30fd922a8d447debc5c299d
Nasir Joseph Guyton-Barreau <nasirjoseph.guyton@students.dominican.edu>
2026-01-14 00:48:38 -0800

    added Json database routes

--- Backend/app.py
+++ Backend/app.py
@@ -1,12 +1,33 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+from datetime import datetime
 
 app = Flask(__name__)
 CORS(app)
 
+practice_session = {
+    "id": 1,
+    "date": datetime.now().strftime("%y-%m-%d"),
+    "notes": ""
+}
+
 @app.route("/api/health")
 def health():
-    return jsonify({"status": "ok"})
+    return jsonify(practice_session)
 
-if __name__ == "__main__":
+
+@app.route("/api/session", methods=["GET"])
+def get_session():
+    return jsonify(practice_session)
+
+
+@app.route("/api/session", methods=["PUT"])
+def update_session():
+    data = request.json
+    practice_session["notes"] = data.get("notes", "")
+    return jsonify(practice_session)
+
+if __name__== "__main__":
     app.run(debug=True)
+
+
